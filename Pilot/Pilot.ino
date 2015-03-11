2c161cc12ea6a72d47e2034be27120d0d64d85a1
Nathan Moos <moosingin3space@gmail.com>
2015-03-11 18:28:08 -0400

    receiver working

--- Pilot/Pilot.ino
+++ Pilot/Pilot.ino
@@ -1,21 +1,38 @@
 #include <Servo.h>
+#include <PinChangeInt.h>
 
 #define SERVO_PORT 7
 #define SERVO_RECEIVER_IN 4
 
 Servo servo;
 
+volatile int pwm_value = 0;
+volatile int prev_time = 0;
+uint8_t pin;
+
+void rising() {
+  pin = PCintPort::arduinoPin;
+  PCintPort::attachInterrupt(pin, &falling, FALLING);
+  prev_time = micros();
+}
+
+void falling() {
+  pin = PCintPort::arduinoPin;
+  PCintPort::attachInterrupt(pin, &rising, RISING);
+  pwm_value = micros()-prev_time;
+  Serial.println(pwm_value);
+  servo.writeMicroseconds(pwm_value);
+}
+
 void setup() {
   // put your setup code here, to run once:
   servo.attach(SERVO_PORT);
-  pinMode(SERVO_RECEIVER_IN, OUTPUT);
+  pinMode(SERVO_RECEIVER_IN, INPUT);
+  digitalWrite(SERVO_RECEIVER_IN, HIGH);
   Serial.begin(9600);
+  PCintPort::attachInterrupt(SERVO_RECEIVER_IN, &rising, RISING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  int pwm_value = pulseIn(SERVO_RECEIVER_IN, HIGH);
-  Serial.println(pwm_value);
-  servo.writeMicroseconds(pwm_value);
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Code for ENGR 100 Blimp
+# Code for ENGR 100 Blimp
+
+## 2015-03-11
+
+The Arduino successfully reads signals from the receiver and successfully
+controls servos. We have established the general form of all additional
+code that will be required for driving.
