e2f797c4084a0ed5edccaac3b2db25f7ca79faed
Nathan Moos <moosingin3space@gmail.com>
2015-03-15 13:42:50 -0400

    added gains support

--- Pilot/Pilot.ino
+++ Pilot/Pilot.ino
@@ -1,38 +1,65 @@
 #include <Servo.h>
 #include <PinChangeInt.h>
 
-#define SERVO_PORT 7
-#define SERVO_RECEIVER_IN 4
+#define NUM_INPUT_CHANNELS 4
+#define NUM_OUTPUT_CHANNELS 4
 
-Servo servo;
+#define INPUT_CHANNEL_OFFSET 2
+#define OUTPUT_CHANNEL_OFFSET 6
 
-volatile int pwm_value = 0;
-volatile int prev_time = 0;
-uint8_t pin;
+#define PWM_MIN 800
+#define PWM_MAX 2100
+
+Servo output[NUM_OUTPUT_CHANNELS];
+volatile int input[NUM_INPUT_CHANNELS];
+volatile int prev_time[NUM_INPUT_CHANNELS];
+int gains[] = { 100, 100, 100, 100 };
+
+int amplify(int gain, int time) {
+  // start by mapping the time to a percentage power scale
+  int pct = map(time, PWM_MIN, PWM_MAX, 0, 100);
+  // Now, multiply by the gain and map it back to a percentage power scale
+  int outPct = map(constrain(pct * gain, 0, 10000), 0, 10000, 0, 100);
+  // Now, map it back to a PWM scale
+  int outTime = map(outPct, 0, 100, PWM_MIN, PWM_MAX);
+  return outTime;
+}
+
+void pass_through_gain(int chan) {
+  int time = amplify(gains[chan], input[chan]);
+  Serial.print(chan);
+  Serial.print(": ");
+  Serial.println(time);
+  output[chan].writeMicroseconds(time);
+}
 
 void rising() {
-  pin = PCintPort::arduinoPin;
+  uint8_t pin = PCintPort::arduinoPin;
   PCintPort::attachInterrupt(pin, &falling, FALLING);
-  prev_time = micros();
+  prev_time[pin - INPUT_CHANNEL_OFFSET] = micros();
 }
 
 void falling() {
-  pin = PCintPort::arduinoPin;
+  uint8_t pin = PCintPort::arduinoPin;
   PCintPort::attachInterrupt(pin, &rising, RISING);
-  pwm_value = micros()-prev_time;
-  Serial.println(pwm_value);
-  servo.writeMicroseconds(pwm_value);
+  input[pin - INPUT_CHANNEL_OFFSET] = micros() - prev_time[pin - INPUT_CHANNEL_OFFSET];
+  pass_through_gain(pin - INPUT_CHANNEL_OFFSET);
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  servo.attach(SERVO_PORT);
-  pinMode(SERVO_RECEIVER_IN, INPUT);
-  digitalWrite(SERVO_RECEIVER_IN, HIGH);
+  // initialize output servos
+  for (int i = 0; i < NUM_OUTPUT_CHANNELS; i++) {
+    output[i].attach(OUTPUT_CHANNEL_OFFSET + i);
+  }
+  // initialize receiver inputs
+  for (int i = 0; i < NUM_INPUT_CHANNELS; i++) {
+    input[i] = 0;
+    prev_time[i] = 0;
+    pinMode(INPUT_CHANNEL_OFFSET + i, INPUT);
+    digitalWrite(INPUT_CHANNEL_OFFSET + i, HIGH);
+    PCintPort::attachInterrupt(INPUT_CHANNEL_OFFSET + i, &rising, RISING);
+  }
   Serial.begin(9600);
-  PCintPort::attachInterrupt(SERVO_RECEIVER_IN, &rising, RISING);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {} // leave this empty, do everything in interrupt handlers
