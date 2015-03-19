4ce1fc813935f0d96337190eabeb15895ade8a23
Nathan Moos <moosingin3space@gmail.com>
2015-03-19 12:26:00 -0400

    added comments

--- Pilot/Pilot.ino
+++ Pilot/Pilot.ino
@@ -1,15 +1,34 @@
+// Used to write PWM signals
 #include <Servo.h>
+// Used to listen for pin-change interrupts
 #include <PinChangeInt.h>
+// Contains blimp-specific configuration
 #include "localdefs.h"
 
+// This macro defines the conversion between
+// a pin number and the channel number
 #define PIN2CHAN(x) x - INPUT_CHANNEL_OFFSET
-#define PWM_MIN 800
+
+// These macros define the range of the 
+// PWM signals that the radio receives and
+// that the servos are sent.
+#define PWM_MIN 900
 #define PWM_MAX 2100
 
+// Each output channel is stored in this array
 Servo output[NUM_OUTPUT_CHANNELS];
+
+// The input PWM signals
 volatile int input[NUM_INPUT_CHANNELS];
+
+// The time of the rising-edge signal.
+// Temporary.
 volatile int prev_time[NUM_INPUT_CHANNELS];
 
+// Amplifies the PWM signal stored in 'time'
+// by the 100-based 'gain' parameter.
+// The offset parameter is applied after
+// the amplification.
 int amplify(int gain, int offset, int time) {
   // start by mapping the time to a percentage power scale
   int pct = map(time, PWM_MIN, PWM_MAX, 0, 100);
@@ -22,44 +41,68 @@ int amplify(int gain, int offset, int time) {
   return outTime;
 }
 
+// Writes a PWM signal to any number of output channels
 void write_servo(int time, int numChans, int chans[]) {
   for (int i = 0; i < numChans; i++) {
+      // get the output channel
       int chan = chans[i];
+      // Serial output for debugging
       Serial.print(chan);
       Serial.print(": ");
       Serial.println(time);
+      // Write the signal using the Servo library
       output[chan].writeMicroseconds(time);
   }
 }
 
+// Called on the rising edge of a PWM signal
 void rising() {
   uint8_t pin = PCintPort::arduinoPin;
+  // Listen for the falling edge on this pin
   PCintPort::attachInterrupt(pin, &falling, FALLING);
+  // Save the time of this event with the channel.
   prev_time[PIN2CHAN(pin)] = micros();
 }
 
+// Called on the falling edge of a PWM signal
 void falling() {
   uint8_t pin = PCintPort::arduinoPin;
+  // Re-register listening for the rising edge
   PCintPort::attachInterrupt(pin, &rising, RISING);
+  // Using the rising-edge time, calculate the width
   input[PIN2CHAN(pin)] = micros() - prev_time[PIN2CHAN(pin)];
+  // Amplify the signal using the associated gain, offset, and input
   int servo_time = amplify(gains[PIN2CHAN(pin)], offsets[PIN2CHAN(pin)], input[PIN2CHAN(pin)]);
+  // Write the amplified signal to the associated output channels
   write_servo(servo_time, numOutputChans[PIN2CHAN(pin)], outputChans[PIN2CHAN(pin)]);
 }
 
+// Initialize the program
 void setup() {
   // initialize output servos
   for (int i = 0; i < NUM_OUTPUT_CHANNELS; i++) {
+    // Associate output pins with channels
     output[i].attach(OUTPUT_CHANNEL_OFFSET + i);
   }
   // initialize receiver inputs
   for (int i = 0; i < NUM_INPUT_CHANNELS; i++) {
+    // Zero all receiver state
     input[i] = 0;
     prev_time[i] = 0;
+    // Set the inputs to input mode and activate 20kohm pull-up resistors.
+    // This is done because otherwise it is difficult to discern pin changes
     pinMode(INPUT_CHANNEL_OFFSET + i, INPUT);
     digitalWrite(INPUT_CHANNEL_OFFSET + i, HIGH);
+    // Attach rising-edge interrupts
     PCintPort::attachInterrupt(INPUT_CHANNEL_OFFSET + i, &rising, RISING);
   }
+  // Open serial port for debugging. Use low-rate serial port to limit CPU time
+  // spent in I/O routines.
   Serial.begin(9600);
 }
 
-void loop() {} // leave this empty, do everything in interrupt handlers
+// This is purposely left empty to avoid synchronization
+// problems. The Interrupt Service Routines (ISRs) defined
+// in 'falling()' and 'rising()' handle the entire program
+// flow - code here will only serve to complicate this.
+void loop() {} 
--- Pilot/localdefs.h
+++ Pilot/localdefs.h
@@ -1,19 +1,44 @@
+// Prevents double-definition issues
 #pragma once
 
+// The number of channels
 #define NUM_INPUT_CHANNELS 3
 #define NUM_OUTPUT_CHANNELS 4
 
+// The channel-to-pin offsets.
+// if INPUT_CHANNEL_OFFSET is 2, then a signal on pin 3 will be interpreted as
+// a signal on channel 1.
 #define INPUT_CHANNEL_OFFSET 2
 #define OUTPUT_CHANNEL_OFFSET 6
 
+// This gain enables the full range-of-motion of a servo without the need for
+// the trim-tabs.
 #define SERVO_GAIN 162
 
-int numOutputChans[] = { 2, 1, 1 };
+// Mapping of the input channels to output channels
+int numOutputChans[] = { 
+    2, // In 0
+    1, // In 1
+    1  // In 2
+};
 int *outputChans[] = {
-    (int[]) { 0, 1 },
-    (int[]) { 2 },
-    (int[]) { 3 }
+    (int[]) { 0, 1 }, // In 0
+    (int[]) { 2 },    // In 1
+    (int[]) { 3 }     // In 2
 };
 
-int gains[] = { 100, 100, 100 };
-int offsets[] = { 0, 0, 0 };
+// Gains for each input channel
+// 100-based
+int gains[] = { 
+    100, // In 0
+    100, // In 1
+    100  // In 2
+};
+// Offsets for each input channel
+// Applied to a signal varying from
+// 0-100
+int offsets[] = { 
+    0, // In 0
+    0, // In 1
+    0  // In 2
+};
